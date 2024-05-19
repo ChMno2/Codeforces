9309c088f25a5541dcffc157d2fe95ad6878b584
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-05-19 12:19:59 +0800

    Create A. Next Round.cpp

---
+++ VK Cup 2012 Qualification Round 1/A. Next Round.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+	for(int i=0;i<n;i++)
+    {
+    	cin>>a[i];
+	}
+	k=a[k-1];
+	int i;
+	for(i=n-1;i>=0;i--)
+	{
+		if(a[i]>=k&&a[i]!=0)
+			break;
+	}
+	if(i<0)
+		cout<<0<<endl;
+	else
+		cout<<i+1<<endl;
+}
+

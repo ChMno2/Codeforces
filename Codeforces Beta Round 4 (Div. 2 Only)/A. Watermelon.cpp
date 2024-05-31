1916a4a3019510a252c361b0cf0fd24be4bdc4cd
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-05-31 15:07:09 +0800

    Update A. Watermelon.cpp

--- Codeforces Beta Round 4 (Div. 2 Only)/A. Watermelon.cpp
+++ Codeforces Beta Round 4 (Div. 2 Only)/A. Watermelon.cpp
@@ -12,3 +12,17 @@ int main() {
 		cout<<"NO"<<endl;
 }
   
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int w;
+    cin>>w;
+    if(w%2==0&&w!=2)
+    	cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+}
+  
